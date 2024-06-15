4ad887d30914aef17b3c2f049d0de4fbf4861921
cuppar <cuppar@163.com>
2024-06-15 18:12:41 +0800

    refine

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Godot New Project Template
+
+## Features
+
+- Automatically switch scenes and fade in and out
+- Todo Management
+- Code Formatter
+- Constants Management
+- Toggle External Editor
--- scenes/main.gd
+++ scenes/main.gd
@@ -3,7 +3,10 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass  # Replace with function body.
+	await get_tree().create_timer(1.0).timeout
+	SceneTranslation.change_scene_to_file("res://scenes/test.tscn")
+	print("c.FOO: ", c.FOO)
+	# TODO: todo something...
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
